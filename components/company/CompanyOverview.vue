61292ae0eeaea4f27f54536f44f48e7a2a2f8f1e
Sash <n.sashmikha@gmail.com>
2023-06-17 15:24:57 +0800

    update

--- components/company/CompanyOverview.vue
+++ components/company/CompanyOverview.vue
@@ -1,15 +1,14 @@
 <template lang="pug">
-    v-card.pa-4.rounded-lg(outlined)
-      .company.fill-width
-        p.subtitle-2.px-4.pt-2.mb-0 Company Overview
-        p.text-h5.px-4.mb-0.pt-0.primary--text Improving operational effectiveness through Intelligent automation, Data Services & ML and Conversational AI
-        br
-        v-card-text.pt-2
-          p.font-weight-medium Intelligent Automation
-          p Working with primarily UiPath and Microsoft (Power Automate/Apps) platforms, supported by other Intelligent Document providers (Abbyy/Google and more) we help our customers realize their automation journey, Our strong background in finance, supply chain, manufacturing and financial services gives us the necessary business understanding to transform and automate processes which makes sense for the business.
-          br
-          p.font-weight-medium Conversational AI
-          p Whereas Robotic Process Automation takes care of execution Low-code Conversational AI engines like our platform, BotExpress, automates the communication through a variety of customer or internal channels. You can test the bot platform by clicking the chat box in lower right hand corner.
+div
+ v-card.pa-4.mb-4.rounded-lg(outlined)
+   .company.fill-width
+       p.subtitle-3.px-4.pt-2.mb-0 Expenses
+       p.subtitle-1.font-weight-regular.ml-4.primary--text from 12/8/2023 to 18/6/2023
+
+ v-card.pa-4.rounded-lg(outlined)
+   .company.fill-width
+       p.subtitle-3.px-4.pt-2.mb-0 Sales
+       p.subtitle-1.font-weight-regular.ml-4.primary--text from 12/8/2023 to 18/6/2023
 </template>
 
 <script>
