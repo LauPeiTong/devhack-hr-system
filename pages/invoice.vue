2aeb3191a5d71ce0acbbb5324482baf05e8dd627
Lau Pei Tong <90409271+LauPeiTong@users.noreply.github.com>
2023-06-18 09:32:38 +0800

    Add

--- pages/invoice.vue
+++ pages/invoice.vue
@@ -2,13 +2,11 @@
   .dashboard-page.pa-0.ma-0.fill-width
     upper-title.ma-0(:title="'Employee'" :titleClass="'dark-background'" :icon="'bell'" :rightIconColor="$vuetify.theme.themes.light.tertiary")
     v-row.ma-0.pt-8.fill-width
-      v-col(cols="8")
-        //- company-details-vue.mt-10.align-center.pl-4.pb-4.mb-5.ml-5
+      v-col(cols="12")
+        //- company-details-vue.align-center.pl-4.pb-4.mb-5.ml-5
         //- company-overview-vue.pb-8.align-center.pl-4.mb-5.ml-5
         //- line-chart-vue.pb-8.align-center.pl-4.mb-5.ml-5
-        invoice-details-vue.pb-8.align-center.pl-4.mb-5.ml-5
-      v-col(cols="4")
-        //- company-status-vue.mt-10.align-center.pr-4.pb-4.mb-5.mr-5
+        invoice-details-vue.mt-10.pb-8.align-center.pl-4.mb-5.ml-5.mr-5
 
   </template>
 
