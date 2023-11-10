11001cb6bb99be07f7c4227001a20ba26c53a780
Lau Pei Tong <90409271+LauPeiTong@users.noreply.github.com>
2023-11-11 00:07:16 +0800

    Added

--- components/chatbot/ChatBot.vue
+++ components/chatbot/ChatBot.vue
@@ -1,59 +1,58 @@
-<!-- eslint-disable no-tabs -->
 <template lang="pug">
 v-card.chatbot.rounded-lg(flat height="640" )
-	v-card.chat.overflow-y-auto(id="scroll" height="520" flat)
-		v-container.pt-4.px-6
-			template(v-for="(c, index) in conversation")
-				.me(v-if="c.isMe")
-					v-row
-						v-col.d-flex.justify-end(col="10")
-							v-card.pa-2.rounded-lg.rounded-tr-0.lightBlue(outlined)
-								p.body-2.wrapped-text {{ c.text }}
+  v-card.chat.overflow-y-auto(id="scroll" height="520" flat)
+    v-container.pt-4.px-6
+      template(v-for="(c, index) in conversation")
+        .me(v-if="c.isMe")
+          v-row
+            v-col.d-flex.justify-end(col="10")
+              v-card.pa-2.rounded-lg.rounded-tr-0.lightBlue(outlined)
+                p.body-2.wrapped-text {{ c.text }}
 
-					v-fade-transition(leave-absolute mode="out-in")
-						v-row.mb-1.mt-1(v-if="index === conversation.length - 1")
-							v-col.d-flex.justify-start(cols="10")
-								v-card.pa-2.rounded-lg.rounded-tl-0(outlined height="240")
-									p.body-2.wrapped-text.grey--text.mb-2 RBot is working on your response.
-									v-img.mx-auto.rounded-pill.mb-1(:src="require(`../../assets/img/think.gif`)" max-height="200" max-width="200")
+          v-fade-transition(leave-absolute mode="out-in")
+            v-row.mb-1.mt-1(v-if="index === conversation.length - 1")
+              v-col.d-flex.justify-start(cols="10")
+                v-card.pa-2.rounded-lg.rounded-tl-0(outlined height="240")
+                  p.body-2.wrapped-text.grey--text.mb-2 RBot is working on your response.
+                  v-img.mx-auto.rounded-pill.mb-1(:src="require(`../../assets/img/think.gif`)" max-height="200" max-width="200")
 
-				.webot(v-else)
-					v-fade-transition(leave-absolute mode="out-in")
-						v-row.mb-1.mt-1
-							v-col.d-flex.justify-start(cols="10")
-								v-card.pa-2.rounded-lg.rounded-tl-0(outlined)
-									.d-flex.justify-end.mb-1
-										v-icon.lightBlue2--text.me-2(small @click="") mdi-content-copy
-										v-icon.lightBlue2--text.me-2(small @click="") mdi-thumb-up-outline
-										v-icon.lightBlue2--text(small @click="") mdi-thumb-down-outline
-									pre.body-2.wrapped-text {{ c.text }}
-							v-col(cols="10" v-if="c.isDefault && c.defaultId == 1 && c.text !== 'Here is my suggestion:\nAn error occurred.'")
-								v-row
-									v-col.pt-0(cols="10")
-										v-card.pa-2.rounded-lg.rounded-tl-0(outlined)
-											p.body-2.wrapped-text.font-weight-medium.mb-0 You can download the file in here to edit the offer letter:
-									v-col.pt-0(cols="3")
-										a.text-decoration-none(:href="require(`~/assets/img/rbot.gif`)" download="OfferLetter.docx")
-											v-card.mx-auto.rounded-lg.pt-4.fill-height.text-center(
-												@click=""
-												outlined
-											)
-												v-card.text-center.rounded-lg.background.mx-16(flat)
-													v-icon.pa-4.primary--text(x-large) mdi-file-document
-												.px-4.py-2
-													p.secondary--text.font-weight-medium.mb-0 Offer Letter
-													p.caption.darkGrey--text.font-weight-light.mb-1 Microsoft Word Document
+        .webot(v-else)
+          v-fade-transition(leave-absolute mode="out-in")
+            v-row.mb-1.mt-1
+              v-col.d-flex.justify-start(cols="10")
+                v-card.pa-2.rounded-lg.rounded-tl-0(outlined)
+                  .d-flex.justify-end.mb-1
+                    v-icon.lightBlue2--text.me-2(small @click="") mdi-content-copy
+                    v-icon.lightBlue2--text.me-2(small @click="") mdi-thumb-up-outline
+                    v-icon.lightBlue2--text(small @click="") mdi-thumb-down-outline
+                  pre.body-2.wrapped-text {{ c.text }}
+              v-col(cols="10" v-if="c.isDefault && c.defaultId == 1 && c.text !== `Here is my suggestion:\nAn error occurred.`")
+                v-row
+                  v-col.pt-0(cols="10")
+                    v-card.pa-2.rounded-lg.rounded-tl-0(outlined)
+                      p.body-2.wrapped-text.font-weight-medium.mb-0 You can download the file in here to edit the offer letter:
+                  v-col.pt-0(cols="3")
+                    a.text-decoration-none(:href="require(`~/assets/img/rbot.gif`)" download="OfferLetter.docx")
+                      v-card.mx-auto.rounded-lg.pt-4.fill-height.text-center(
+                        @click=""
+                        outlined
+                      )
+                        v-card.text-center.rounded-lg.background.mx-16(flat)
+                          v-icon.pa-4.primary--text(x-large) mdi-file-document
+                        .px-4.py-2
+                          p.secondary--text.font-weight-medium.mb-0 Offer Letter
+                          p.caption.darkGrey--text.font-weight-light.mb-1 Microsoft Word Document
 
-	v-footer.white.rounded-t-xl(absolute elevation="0")
-		v-col(cols="12")
-			v-textarea.rounded-xl.message-text-field(outlined auto-grow hide-details rows="1"
-				v-model="message"
-				label="YYou can ask RBot to streamline recruitment processes"
-				:color="$vuetify.theme.themes.light.primary"
-				append-outer-icon="mdi-send-circle"
-				@keydown=""
-				v-on:keyup.enter="sendMessage"
-				@click:append-outer="sendMessage")
+  v-footer.white.rounded-t-xl(absolute elevation="0")
+    v-col(cols="12")
+      v-textarea.rounded-xl.message-text-field(outlined auto-grow hide-details rows="1"
+        v-model="message"
+        label="YYou can ask RBot to streamline recruitment processes"
+        :color="$vuetify.theme.themes.light.primary"
+        append-outer-icon="mdi-send-circle"
+        @keydown=""
+        v-on:keyup.enter="sendMessage"
+        @click:append-outer="sendMessage")
 
 </template>
 <script>
